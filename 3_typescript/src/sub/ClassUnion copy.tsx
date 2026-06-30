796075e379d2182b8e983611cabb05d062d05852
RH <rhhhkh@gmail.com>
2026-07-01 01:11:50 +0900

    Partial<T>

--- 3_typescript/src/sub/ClassUnion copy.tsx
+++ 3_typescript/src/sub/ClassUnion copy.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 
 //1.원본데이터 모든값이 필수 입력사항
 interface UserProfile {
@@ -9,29 +10,63 @@ interface UserProfile {
 }
 
 //2가상의 유저 데이터
+export const ClassUnion = () => {
 
-let currentUser: Userprofile ={
+const [user, setUser] = useState<UserProfile>({
     id: "user_!23",
-    username: "현진",
+    name: "황현진",
     email: "skz@idol.com",
     bio: "스키즈 메덴",
     theme: "light"
-}
+});
+
 // 3. 🌟 Partial을 사용한 수정 함수
 //Partial<UserProfile> 덕분에 
 // id, username 등을 다 안 보내고 "바꾸고 싶은 것만" 보낼 수 있어!
 const updateUserProfile = (changes: Partial<UserProfile>) =>{
-    currentUser ={
-        ...currentUser,
-        ...changes //기존 유저 정보에 바뀐 부분만 덮어쓰기!!
-    };
-    console.log("수정완료! 현재프로필:", currentUser); 
-}
+    setUser( prev=>({
+            ...prev, //기존데이터 펼치기 
+            ...changes //✨기존 유저 정보에 바뀐 부분만 덮어쓰기!!
+    }));
+};
 
-//4.실제 함수호출하기
-//다크모드로 바꾸고 자기소개만 수정하고 싶을떄 이것만 넘겨줘도 에러안남
+return(
+    <>
+    <div style={{padding:"20px", border:"1px solid #ccc", 
+        borderRadius:"10px", margin:"20px 0"
+    }}>
+        <h2>👤 내 프로필(Partial test)</h2>
+        <pre style={{background:"#f4f4f4", padding:"15px",
+                    borderRadius:"5px" }}>
+        {JSON.stringify(user, null, 2)}
+        </pre>
 
-updateUserProfile({
-    theme: "dark",
-    bio: "🚀 스키즈 "
-})
+        <hr style={{margin:"20px 0"}}/>
+
+        <h3>👇 버튼을 눌러 프로필의 "일부"만 수정해 보세요!</h3>
+
+        <button onClick={()=>updateUserProfile({
+                                theme: "dark",
+                                bio: "🚀 스키즈 "
+        })}
+        style={{marginRight: "10px", padding: "10px", cursor:"pointer"}}
+        > update </button>
+
+        <button 
+        onClick={()=>updateUserProfile({name: "디올왕자"})}
+        style={{marginRight: "10px", padding: "10px",  cursor:"pointer"}}
+        > 디올왕자버튼 </button>
+        <button
+        onClick={()=>setUser({
+            id: "user_!23",
+            name: "황현진",
+            email: "skz@idol.com",
+            bio: "스키즈 메덴",
+            theme: "light"
+        })}
+        style={{ border: '1px solid #ccc', padding: '10px', cursor: 'pointer', background: '#eee' }}
+        >초기화</button>
+    </div>
+    </>
+)
+};
